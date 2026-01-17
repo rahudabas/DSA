11bce18f3412bb55322eadf193f13b0d8e0f1fea
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-01-17 13:51:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.6 MB (49.08%)

---
+++ 0075-sort-colors/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0 ,mid =0;
+        int high = nums.size()-1;
+
+        while(high>=mid){
+            if(nums[mid]==0){
+                nums[mid]=nums[low];
+                nums[low]=0;
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                nums[mid]=nums[high];
+                nums[high]=2;
+                high--;
+            }
+        }
+    }
+};
