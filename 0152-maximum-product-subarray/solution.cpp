9e6a1c87aba249d58ba288ac447919ff72132f82
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-05-26 18:10:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (23.80%)

---
+++ 0152-maximum-product-subarray/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int suf=1,pref=1;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(suf==0)suf=1;
+            if(pref==0)pref=1;
+
+            suf=suf*nums[i];
+            pref=pref*nums[n-i-1];
+            maxi=max(max(suf,pref),maxi);
+
+        }
+    return maxi;
+    }
+};
