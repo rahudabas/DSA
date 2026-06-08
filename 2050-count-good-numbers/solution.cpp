818414913297f2f1b199d9d1df4a3335c845efe0
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-06-08 14:27:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (71.83%)

--- 2050-count-good-numbers/solution.cpp
+++ 2050-count-good-numbers/solution.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    const int mod = 1e9 + 7;
+    const long long mod=1e9+7;
 
+    long long pow(long long a,long long b){
+        if(b==0)return 1;
 
-    long long power(long long x,long long y){
-        long long res=1;
+        long long half=pow(a,b/2);
 
-        x=x%mod;
-
-        while(y>0){
-            if(y%2==1){
-                res=(res*x) %mod;
-            }
-            x= (x*x) %mod;
-            y=y/2;
+        long long result=half*half % mod;
+        if(b%2==1){
+            result=result*a %mod;
         }
-        return res;
-    }    
+        return result;
+    }
 
     int countGoodNumbers(long long n) {
-        long long a;
-        long long even_index=(n+1)/2 ;
-        long long odd_index=n/2;
-
-        long long evenPart = power(5, even_index);
-        long long oddPart = power(4, odd_index);
-
-        return (evenPart* oddPart) %mod;
-        
-        }
+        long long x=5;
+        long long y=4;
+        return pow(x,(n+1)/2)*pow(y,n/2) % mod;
+    }
 };
