af811e63fd9aa6f13acc6427ad8ac6a538aafb5d
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-06-08 12:43:58 +0000

    Sync LeetCode submission Runtime - 4 ms (4.90%), Memory - 9.2 MB (55.32%)

--- 0008-string-to-integer-atoi/solution.cpp
+++ 0008-string-to-integer-atoi/solution.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
+    int helper(const string& s,int i,long long nums, int sign){
+        //base case
+        if(i>=s.size() || !isdigit(s[i])){
+            return int(nums*sign);
+        }
+
+        nums=nums*10+(s[i]-'0');
+
+        if(nums*sign <=INT_MIN)return INT_MIN;
+        if(nums*sign >=INT_MAX)return INT_MAX;
+
+        return helper(s,i+1,nums,sign);
+    }
+    
     int myAtoi(string s) {
-        bool came = false;
-        string numStr = "";
+        int i=0;
+        //for to skip whitespace
+        while(i<s.size() && s[i]==' ' )i++;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == ' ' && came == false) {
-                continue;
-            }
-            else if (s[i] == '-' && came == false) {
-                numStr += s[i];
-                came = true;
-            }
-            
-            else if (s[i] == '+' && came == false) {
-                numStr += s[i];
-                came = true;
-            }
-
-            else if (isdigit(s[i])) {
-                came = true;
-                numStr += s[i];
-            }
-            else {
-                break;
-            }
+        //for doingsign checking 
+        int sign=1;
+        if(i<s.size() && (s[i]=='+' || s[i]=='-')){
+            sign=(s[i]=='-')?-1:1;
+            i++;
         }
-
-        if (numStr == "-" || numStr.empty()) return 0;
-        else if (numStr == "+") return 0;
-
-        try {
-            return stoi(numStr);
-        } catch (...) {
-            return (numStr[0] == '-') ? INT_MIN : INT_MAX;
-        }
+        long long num=0;
+        //helper function
+        return helper(s,i,num,sign);
     }
 };
-
