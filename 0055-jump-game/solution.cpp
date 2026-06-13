74786f993c9f9467116b7cd88089340524f01950
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-06-13 21:12:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 52.2 MB (98.64%)

---
+++ 0055-jump-game/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxind=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>maxind)return false;
+            maxind=max(i+nums[i],maxind);
+        }
+        return true;
+    }
+};
