2360313f85f4ebf4834cb21d263eb017dba72b05
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-06-27 23:04:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (68.22%)

---
+++ 0207-course-schedule/solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+//in it need to find cycle , if for courses prerequisite there becomes a cycle than those courses cant be taken
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //yeh edges ka collection diya h jisme 0->1
+        int n=numCourses;
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n,0);
+
+        for(auto &it : prerequisites){
+            int course = it[0];
+            int prereq = it[1];
+
+            adj[prereq].push_back(course);
+            indegree[course]++;
+        }   
+
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it:adj[node]){//hm jisko topo m daal diye uske adjacent k indegree reduce kro
+                indegree[it]--;//jb remove krte toh adjacent ki indegree reduce krte
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(topo.size()==n)return true;
+        return false;
+    }
+};
