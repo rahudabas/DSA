16a05cf1c34b9edd16bbb938eaacbc0ab67765c2
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-12 16:31:18 +0000

    Sync LeetCode submission Runtime - 163 ms (27.79%), Memory - 111.5 MB (13.73%)

--- 0416-partition-equal-subset-sum/solution.cpp
+++ 0416-partition-equal-subset-sum/solution.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    bool rec(int ind,int target,vector<int>& arr,vector<vector<int>>& dp){
-        int n=arr.size();
-        if(ind==n){
-            if(target==0)return true;
-            return false;
-        }
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        if(target==0)return true;
-        bool take=false;
-        if(arr[ind]<=target)take=rec(ind+1,target-arr[ind],arr,dp);
-        bool nottake=rec(ind+1,target,arr,dp);
+    int rec(int i,int target,vector<int>&nums, vector<vector<int>>& dp){
+        int n=nums.size();
+        if(target==0)return dp[i][target]=true;
 
-        return dp[ind][target]=take || nottake;
+        if(i==n)return dp[i][target]=false;
+        int take,nottake;
+
+        if(dp[i][target]!=-1)return dp[i][target];
+        if(nums[i]>target)take=false;
+        else take=rec(i+1,target-nums[i],nums,dp);
+
+        nottake=rec(i+1,target,nums,dp);
+
+        return dp[i][target]=take || nottake;
     }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
-        int target=0;
+        int sum=0;
         for(int i=0;i<n;i++){
-            target+=nums[i];
+            sum+=nums[i];
         }
-        if(target%2==1)return false;
-        target=target/2;
+        int target;
+        if(sum%2==1)return false;
+        target=sum/2;
+
         vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
+
         return rec(0,target,nums,dp);
     }
 };
