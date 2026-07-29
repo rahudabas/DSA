a578eeccb7d318959a9bf80c552e720a32d57405
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-29 20:02:35 +0000

    Sync LeetCode submission Runtime - 7 ms (38.31%), Memory - 23.8 MB (59.66%)

--- 0056-merge-intervals/solution.cpp
+++ 0056-merge-intervals/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        ans.push_back(intervals[0]);
         int n=intervals.size();
-
-        for(int i=0;i<n;i++){
-            if(ans.empty() || intervals[i][0]>ans.back()[1]){
-                ans.push_back(intervals[i]);
+        for(int i=1;i<n;i++){
+            if(ans.back()[1]>=intervals[i][0]){
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
             else{
-                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+                ans.push_back(intervals[i]);
             }
         }
-    return ans;
+        return ans;
     }
 };
