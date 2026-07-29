b0a79e4f2829663ed196ee16714e8aac282ad616
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-29 18:36:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.7 MB (77.21%)

--- 0229-majority-element-ii/solution.cpp
+++ 0229-majority-element-ii/solution.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cnt1=0, cnt2=0;
-        int ele1;
-        int ele2;
+        int ele1,ele2;
+        int cnt1=0,cnt2=0;
         int n=nums.size();
 
         for(int i=0;i<n;i++){
             if(cnt1==0 && nums[i]!=ele2){
-                ele1=nums[i];
                 cnt1++;
+                ele1=nums[i];
             }
             else if(cnt2==0 && nums[i]!=ele1){
+                cnt2++;
                 ele2=nums[i];
-                cnt2++;
             }
-            else if(nums[i]==ele1){
-                cnt1++;
-            }
-            else if(nums[i]==ele2){
-                cnt2++;
-            }
+            else if(nums[i]==ele1)cnt1++;
+            else if(nums[i]==ele2)cnt2++;
             else{
                 cnt1--;
                 cnt2--;
@@ -28,14 +23,13 @@ public:
         }
         cnt1=0;
         cnt2=0;
-
         for(int i=0;i<n;i++){
-            if(nums[i]==ele1)cnt1++;
-            if(nums[i]==ele2)cnt2++;
+            if(ele1==nums[i])cnt1++;
+            else if(ele2==nums[i])cnt2++;
         }
         vector<int> ans;
         if(cnt1>n/3)ans.push_back(ele1);
-        if(cnt2>n/3 && ele1!=ele2)ans.push_back(ele2);
+        if(cnt2>n/3)ans.push_back(ele2);
         return ans;
     }
 };
