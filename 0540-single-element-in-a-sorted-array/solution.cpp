3d5640e108fa93390a3ac724002ebc5f035a5156
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-31 13:17:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.2 MB (36.31%)

--- 0540-single-element-in-a-sorted-array/solution.cpp
+++ 0540-single-element-in-a-sorted-array/solution.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int low=0, high=nums.size()-1;
-
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        if(n==1)return nums[0];
         while(low<=high){
-
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
+            if(mid==0)return nums[0];
             
-            if(mid==0){
-                return nums[0];
-            }
-            if(mid!=0 && nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
-
+            if(nums[mid]!=nums[mid-1]&& nums[mid]!=nums[mid+1])return nums[mid];
             if(mid%2==0){
                 if(nums[mid]==nums[mid-1]){
                     high=mid-1;
                 }
-                else{
-                    low=mid+1;
-                }
+                else low=mid+1;
             }
             else{
-                if(nums[mid]==nums[mid-1]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
+                if(nums[mid]==nums[mid-1])low=mid+1;
+                else high=mid-1;
             }
         }
-    return -1;
+        return -1;
     }
 };
