28beaeba8e493b5ad427ead710a3f2ce630befe0
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-31 14:01:07 +0000

    Sync LeetCode submission Runtime - 7 ms (69.62%), Memory - 22.9 MB (87.32%)

--- 0907-koko-eating-bananas/solution.cpp
+++ 0907-koko-eating-bananas/solution.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    long long hrtaken(vector<int>& piles, int mid){
+    long long rec(int mid,vector<int>& piles){
         long long cnt=0;
-        for(int i=0;i<piles.size();i++){
-            cnt+=(piles[i]+mid-1)/mid; 
+        int n=piles.size();
+        for(int i=0;i<n;i++){
+            if(piles[i]%mid==0)cnt+=piles[i]/mid;
+            else cnt+=piles[i]/mid +1;
         }
         return cnt;
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low=1,high=*max_element(piles.begin(),piles.end());
+        int n=piles.size();
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
 
         while(low<=high){
-            int mid=(low+high)/2;
-
-            long long k=hrtaken(piles,mid);//hrs taken to eat banana with speed=mid
-
-            if(k<=h){
-                high=mid-1;
-            }
+            int mid=low+(high-low)/2;
+            if(rec(mid,piles)<=h)high=mid-1;
             else low=mid+1;
         }
-    return low;
+        return low;
     }
 };
