5f3a540ae06fe045c0abcd2265c7e5ac19fc7b37
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-31 15:02:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.1 MB (93.18%)

--- 1646-kth-missing-positive-number/solution.cpp
+++ 1646-kth-missing-positive-number/solution.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int low=0, high=arr.size()-1;
+        int n=arr.size();
 
-        while(low<=high){
-            int mid=(low+high)/2;
-            int missing=arr[mid]-(mid+1);
-            if(missing<k){
-                low=mid+1;
-            }//mtlb abhi tk k s km mising element h
-            else{
-                high=mid-1;
-            }
+        for(int i=0;i<n;i++){
+            if(arr[i]<=k)k++;
+            else return k;
         }
-
-        return high+1+k;
+        return k;
     }
 };
