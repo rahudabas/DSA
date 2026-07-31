19b4adca64734a6b8f0a301cc54e18622246c803
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-31 11:34:24 +0000

    Sync LeetCode submission Runtime - 3 ms (1.57%), Memory - 15.2 MB (69.08%)

--- 0033-search-in-rotated-sorted-array/solution.cpp
+++ 0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1;
+       int n=nums.size();
+       int low=0;
+       int high=n-1;
 
-        while(low<=high){
-            int mid=(low+high)/2;
+       while(low<=high){
+        int mid=low+(high-low)/2;
 
-            if(nums[mid]==target){
-                return mid;
+        if(nums[mid]==target)return mid;
+        else if(nums[low]<=nums[mid]){
+            if(nums[mid]>target && nums[low]<=target){
+                high=mid-1;
             }
-
-            //if left part is sorted
-            if(nums[low]<=nums[mid]){
-                if(nums[mid]>target && target>=nums[low]){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-            //if right part is sorted
-            else{
-                if(nums[mid]<target && target<=nums[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-            }
+            else low=mid+1;
         }
-    return -1;
+        else{
+            if(nums[mid]<target && nums[high]>=target){
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+       } 
+       return -1;
     }
 };
