c886106c6f9bc7534c235a71c2249d6386abd9cc
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-31 11:18:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (54.43%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
+    int lb(vector<int>& nums,int target){
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=target){
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return low;
+    }
+    
+    int ub(vector<int>& nums,int target){
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<=target)low=mid+1;
+            else if(nums[mid]>target)high=mid-1;
+        }
+        return low;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1;
-        vector<int> ans;
-        int k=-1;
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            //lower bound approach         
-            if(nums[mid]==target){
-                k=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)return {lb(nums,target),ub(nums,target)-1};
+            else if(nums[mid]>target)high=mid-1;
+            else low=mid+1;
         }
-        ans.push_back(k);
-        int l=-1;
-        low=0;
-        high=nums.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            //lower bound approach         
-            if(nums[mid]==target){
-                l=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        ans.push_back(l);
-    return ans;
+        return {-1,-1};
     }
 };
