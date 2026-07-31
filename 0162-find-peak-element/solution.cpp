02c7b5d5e910cc61e8d92b960d9b6b708170f86f
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-31 13:30:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.7 MB (7.62%)

--- 0162-find-peak-element/solution.cpp
+++ 0162-find-peak-element/solution.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int low=1;int high=nums.size()-2;
-            if(nums.size()==1)return 0;
-            if(nums[0]>nums[1])return 0;
-            if(nums[nums.size()-1]>nums[nums.size()-2])return nums.size()-1;
+        int n=nums.size();
+        if(n>1 && nums[0]>nums[1])return 0;
+        if(n>1 && nums[n-1]>nums[n-2])return n-1;
 
+        int low=1;
+        int high=n-2;
+        if(n==1)return 0;
         while(low<=high){
             int mid=low+(high-low)/2;
+            int l= mid==0 ? nums[0] : nums[mid-1];
+            int h= mid==n-1 ? nums[n-1] : nums[mid+1];
 
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-                return mid;
-            }
-            else if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1]){
-                //upslope h yeh and right side m h peak then
-                low=mid+1;
-            }
-            else if(nums[mid]<nums[mid-1] && nums[mid]>nums[mid+1]){
-                //downslope h yeh and left side m h peak
-                high=mid-1;
-            }
-            else if(nums[mid]<nums[mid-1] && nums[mid]<nums[mid+1]){
-                //minima pr h and dono side ho sakti h peak so kahi bhi chle jao
-                high=mid-1;
-            }
+            if(nums[mid]>l && nums[mid]>h)return mid;
+            else if(nums[mid]>l)low=mid+1;
+            else high=mid-1;
         }
-    return -1;
+        return -1;
     }
 };
