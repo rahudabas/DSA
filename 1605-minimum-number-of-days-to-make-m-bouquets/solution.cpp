1f59b775cd83814f0fc04bf7b9ae6c7c61fb41e2
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-31 14:31:05 +0000

    Sync LeetCode submission Runtime - 18 ms (83.62%), Memory - 70.6 MB (12.46%)

--- 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    bool possible(vector<int>& arr,int day,int m,int k){
-        int noB=0;
+    int rec(vector<int>& nums,int k,int mid){
         int cnt=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]<=day){
-                //flower has bloomed
-                cnt++;
-            }
+        cnt= nums[0]<=mid ? 1:0;
+        int ans=0;
+        int n=nums.size();
+
+        for(int i=1;i<n;i++){
+            if(nums[i]<=mid)cnt++;
             else{
-                //adding up bloomed consecutive flowers that can make buoquets
-                noB+=cnt/k;
-                cnt=0;
+                //mtlb consecutive loop tut gya;
+                ans+=cnt/k;
+                cnt=0; //reset the bloom cnt
             }
         }
-        noB+=cnt/k;
-
-        if(noB>=m)return true;
-        else return false;
-        return false;
+        ans+=cnt/k;
+        return ans;
     }
-
+    
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int low=*min_element(bloomDay.begin(),bloomDay.end()), high = *max_element(bloomDay.begin(),bloomDay.end());
+        int n=bloomDay.size();
+        int low=1;
+        int high=*max_element(bloomDay.begin(),bloomDay.end());
 
+        if(1ll*m*k > n)return -1;
         while(low<=high){
             int mid=low+(high-low)/2;
 
-            if(1LL * m * k > bloomDay.size()) return -1;
-            if(possible(bloomDay,mid,m,k)==true){
-                high=mid-1;
-                //cause we need minimum days
-            }
-            else{
-                low=mid+1;
-            }
-        }
-    return low;
+            if(rec(bloomDay,k,mid)>=m)high=mid-1;
+            else low=mid+1;
+        }   
+        return low;
     }
 };
