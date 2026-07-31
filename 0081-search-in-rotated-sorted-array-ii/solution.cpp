c5236f791148b360809f951a11ca97f846a2853c
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-07-31 11:36:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (8.62%)

--- 0081-search-in-rotated-sorted-array-ii/solution.cpp
+++ 0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1;
+               int n=nums.size();
+       int low=0;
+       int high=n-1;
 
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){
-                return true;
-                }
-            //there can be one condition when low=mid=high, due to duplicacy so we not able to find sorted or not, so we trim the array
+       while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target)return true;
 
-            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
-                low=low+1;
-                high=high-1;
-                continue;
-                //this continues trimming
-            }
-            //check left half sorted
-            if(nums[low]<=nums[mid]){
-                if(target<=nums[mid] && target>=nums[low]){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-            //right half sorted
-            else{
-                if(target>=nums[mid] && target<=nums[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-
-            }
+        if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+            low=low+1;
+            high--;
         }
-        return false;
+        else if(nums[low]<=nums[mid]){
+            if(nums[mid]>target && nums[low]<=target){
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        else{
+            if(nums[mid]<target && nums[high]>=target){
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+       } 
+       return false;
     }
 };
