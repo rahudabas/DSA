9215dc429e4d0ebde3765347d54a8c2e95893122
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-08-08 17:50:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.1 MB (55.32%)

--- 0008-string-to-integer-atoi/solution.cpp
+++ 0008-string-to-integer-atoi/solution.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    int helper(const string& s,int i,long long nums, int sign){
-        //base case
-        if(i>=s.size() || !isdigit(s[i])){
-            return int(nums*sign);
-        }
+    long long rec(string& s,int i,long long num,int sign){
+        if(i>=s.size() || !isdigit(s[i]) )return int(sign*num);
 
-        nums=nums*10+(s[i]-'0');
+        num=num*10 + (s[i]-'0');
+        if(num*sign <=INT_MIN)return INT_MIN;
+        if(num*sign >=INT_MAX)return INT_MAX;
 
-        if(nums*sign <=INT_MIN)return INT_MIN;
-        if(nums*sign >=INT_MAX)return INT_MAX;
-
-        return helper(s,i+1,nums,sign);
+        return rec(s,i+1,num,sign);
     }
-    
     int myAtoi(string s) {
         int i=0;
-        //for to skip whitespace
-        while(i<s.size() && s[i]==' ' )i++;
-
-        //for doingsign checking 
+        while(i<s.size() && s[i]==' ')i++;
         int sign=1;
-        if(i<s.size() && (s[i]=='+' || s[i]=='-')){
-            sign=(s[i]=='-')?-1:1;
+        if(i<s.size() &&(s[i]=='+' || s[i]=='-')){
+            sign=(s[i]=='-')?-1 :1;
             i++;
         }
         long long num=0;
-        //helper function
-        return helper(s,i,num,sign);
+        return rec(s,i,num,sign);
     }
 };
