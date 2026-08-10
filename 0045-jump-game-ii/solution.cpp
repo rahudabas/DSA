aa9074644c49ac4b7b2638c6bde5d685861268dc
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-08-10 18:15:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.6 MB (79.13%)

--- 0045-jump-game-ii/solution.cpp
+++ 0045-jump-game-ii/solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int l=0,r=0, jumps=0;
-
-        while(r<nums.size()-1){
-            int farthest=0;
+        int n=nums.size();
+        int l=0,r=0;
+        int jumps=0;
+        while(r<n-1){
+            int fartest=0;
             for(int i=l;i<=r;i++){
-                farthest= max(i+nums[i], farthest);
+                fartest=max(i+nums[i],fartest);
             }
-            jumps++;
             l=r+1;
-            r=farthest;
+            r=fartest;
+            jumps++;
         }
-    return jumps;
+        return jumps;
     }
 };
