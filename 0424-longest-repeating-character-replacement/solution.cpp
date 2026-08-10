39edcda60b7531fe8fae579ffcc920ea7b8c9253
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-08-10 13:14:11 +0000

    Sync LeetCode submission Runtime - 4 ms (57.01%), Memory - 11 MB (19.51%)

--- 0424-longest-repeating-character-replacement/solution.cpp
+++ 0424-longest-repeating-character-replacement/solution.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l=0, r=0, maxlen=0, maxf=0;
-        vector<int> hash(26,0);
-
-        while(r<s.size()){
-            hash[s[r]-'A']++; //updates the hash
-            maxf=max(maxf,hash[s[r]-'A']);
-            int len=r-l+1;
-            if(len-maxf>k){ //allowed s jyada changes h
-                hash[s[l]-'A']--; //left most ko htayege hash s and window ko agge shift krege
-                l=l+1;
+        unordered_map<int,int> mpp;
+        int n=s.length();
+        int l=0,r=0;
+        int maxi=0;
+        int maxf=0;
+        while(r<n){
+            mpp[s[r]-'a']++;
+            maxf=max(maxf,mpp[s[r]-'a']);
+            if((r-l+1)-maxf>k){
+                mpp[s[l]-'a']--;
+                // if(mpp[s[l]-'a']==0){
+                //     mpp.erase(s[l]-'A');
+                // }
+                l++;
             }
-            if(r-l+1 -maxf<=k){
-                maxlen=max(r-l+1,maxlen);
+            else{
+                maxi=max(maxi,r-l+1);
             }
             r++;
         }
-    return maxlen;
+        return maxi;
     }
 };
