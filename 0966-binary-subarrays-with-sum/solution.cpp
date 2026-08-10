fbdfbb37b223f19129779a755fe22124e56dd9f1
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-08-10 13:27:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 32.7 MB (63.50%)

--- 0966-binary-subarrays-with-sum/solution.cpp
+++ 0966-binary-subarrays-with-sum/solution.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int atmost(vector<int>& nums,int goal){
-        if(goal<0)return 0;
+    int sumcnt(vector<int>& nums,int k){
+        if(k<0)return 0;
+        int n=nums.size();
         int l=0,r=0;
-        int sum=0, cnt=0;
-
-        while(r<nums.size()){
+        int sum=0;
+        int cnt=0;
+        while(r<n){
             sum+=nums[r];
-            while(sum>goal){
-                sum=sum-nums[l];
-                l=l+1; // jb sum bda ho toh window ko shrink krege na bhosde
+            while(sum>k){
+                sum-=nums[l];
+                l++;
             }
-            cnt=cnt+(r-l+1);// abb sum shi hogya toh jitna subarray h usko add krege na
-            
-            r=r+1;
+            cnt+=r-l+1;
+            r++;
         }
         return cnt;
     }
-
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-    int cnt1=atmost(nums,goal);
-    int cnt2=atmost(nums,goal-1);
-    return cnt1-cnt2;
+        int cnt1=sumcnt(nums,goal);
+        int cnt2=sumcnt(nums,goal-1);
+        return cnt1-cnt2;
     }
 };
