cdc084510e1f605abe2d6cee15fd9817a788b1f6
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-08-10 13:40:28 +0000

    Sync LeetCode submission Runtime - 7 ms (67.53%), Memory - 71.3 MB (99.92%)

--- 1370-count-number-of-nice-subarrays/solution.cpp
+++ 1370-count-number-of-nice-subarrays/solution.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    int oddfunc(int x){
-        if(x%2==0)return 0;
-        return 1;
-    }
-    int atmost(vector<int>& nums,int k){
+    int niceji(vector<int>& nums,int k){
         if(k<0)return 0;
-
+        int n=nums.size();
         int l=0,r=0;
-        int cnt=0, ans=0;
-
-        while(r<nums.size()){
-            cnt = (oddfunc(nums[r]) == 1) ? cnt + 1 : cnt;
-            
+        int ans=0;
+        int cnt=0;
+        while(r<n){
+            if(nums[r]%2==1)cnt++;
             while(cnt>k){
-                cnt = (oddfunc(nums[l])==1) ? cnt-1:cnt;
-                l++;                
+                if(nums[l]%2==1)cnt--;
+                l++;
             }
-
-            ans=ans+(r-l+1);
-            r=r+1;
+            ans+=(r-l+1);
+            r++;
         }
         return ans;
     }
-
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return atmost(nums,k)-atmost(nums,k-1);
+        return niceji(nums,k)-niceji(nums,k-1);
     }
 };
