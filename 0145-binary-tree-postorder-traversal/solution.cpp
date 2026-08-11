9e657eedf6ed91a4dbb1e2c9f81fbca007926c7f
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-08-11 19:41:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.1 MB (45.97%)

--- 0145-binary-tree-postorder-traversal/solution.cpp
+++ 0145-binary-tree-postorder-traversal/solution.cpp
@@ -11,16 +11,22 @@
  */
 class Solution {
 public:
-    void postord(TreeNode* root,vector<int>& ans){
-        if(root==NULL)return;
-
-        postord(root->left,ans);
-        postord(root->right,ans);
-        ans.push_back(root->val);
-    }
     vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st1,st2;
         vector<int> ans;
-        postord(root,ans);
+        if(root==NULL)return ans;
+        st1.push(root);
+        while(!st1.empty()){
+            root=st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left!=NULL)st1.push(root->left);
+            if(root->right!=NULL)st1.push(root->right);
+        }
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
         return ans;
     }
 };
