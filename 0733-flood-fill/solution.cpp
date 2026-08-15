435431c039e831a7030955abadc674bc586beea9
Rahul Dabas <147968612+rahudabas@users.noreply.github.com>
2026-08-15 22:10:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.5 MB (14.11%)

--- 0733-flood-fill/solution.cpp
+++ 0733-flood-fill/solution.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    void dfs(int row,int col,vector<vector<int>>& image,vector<vector<int>>& vis,int ogcol,int color){
-        vis[row][col]=1;
+    void bfs(vector<vector<int>>& image,vector<vector<int>>& vis,int sr,int sc,int color){
+        int org=image[sr][sc];
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
         int n=image.size();
-        int m=image[0].size();  
-        int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,1,0,-1};
-
-        for(int i=0;i<4;i++){
-            //isse hm neighbours m check krege
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            
-            if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && !vis[nrow][ncol] && image[nrow][ncol]==ogcol){
-                image[nrow][ncol]=color;
-                vis[nrow][ncol]=1;
-                dfs(nrow,ncol,image,vis,ogcol,color);
+        int m=image[0].size();
+        image[sr][sc]=color;
+        int drow[]={1,0,-1,0};
+        int dcol[]={0,1,0,-1};
+        while(!q.empty()){
+            auto row=q.front().first;
+            auto col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && !vis[nrow][ncol] && image[nrow][ncol]==org){
+                    vis[nrow][ncol]=1;
+                    image[nrow][ncol]=color;
+                    q.push({nrow,ncol});
+                }
             }
         }
     }
-
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int ogcol=image[sr][sc];
-        image[sr][sc]=color;
         int n=image.size();
         int m=image[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
-
-        dfs(sr,sc,image,vis,ogcol,color);
+        bfs(image,vis,sr,sc,color);
         return image;
     }
 };
